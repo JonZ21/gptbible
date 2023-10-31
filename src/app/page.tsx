50d90d430e95eb42adc1a39756d1b77e00eb83fa
Jonathan Zhou <jonathanzhou8@gmail.com>
2023-10-31 19:23:16 -0400

    added link and button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="h-screen bg-gradient-to-tr from-blue-500 to-green-500 flex justify-center items-center">
       <motion.div
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, y: -100 }}
+        animate={{ opacity: 1, y: -5  0 }}
         transition={{ duration: 1 }}
       >
         <div className="cursor-pointer flex flex-row items-center">
@@ -21,7 +22,14 @@ export default function Home() {
             <h1 className="text-9xl font-semibold mb-0">BibleGPT</h1>
             <h3 className=" text-5xl mt-0">AI-Powered Bible Tool</h3>
           </div>
-        </div>{" "}
+        </div>
+        <div className="flex flex-row justify-center">
+          <Link href="/books">
+            <button className="bg-white rounded-md text-3xl text-black opacity-90">
+              Get Started
+            </button>
+          </Link>
+        </div>
       </motion.div>
     </div>
   );
