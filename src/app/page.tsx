93fb1a8e1fd564c916a7feb5c9d8a51ff536c057
Jonathan Zhou <jonathanzhou8@gmail.com>
2023-10-31 18:57:08 -0400

    building out front end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=" bg-red-700">
-      <div></div>
-    </main>
+    <div className="h-screen bg-gradient-to-tr from-blue-500 to-green-500 flex justify-center items-center">
+      <div className="cursor-pointer flex flex-row items-center">
+        <Image
+          src="/logos/logo_transparent.png"
+          width={230}
+          height={230}
+          alt="biblelogo"
+        />
+        <div className="flex flex-col">
+          <h1 className="text-9xl font-semibold mb-0">BibleGPT</h1>
+          <h3 className=" text-5xl mt-0">AI-Powered Bible Tool</h3>
+        </div>
+      </div>
+    </div>
   );
 }
