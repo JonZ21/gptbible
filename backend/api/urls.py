99010a1081e9e3eaf50a8d1b09041b41caf41556
Jonathan Zhou <jonathanzhou8@gmail.com>
2023-10-31 17:14:26 -0400

    hid api key

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('get_Chapter/<str:book>/<int:chapter>/', views.get_chapter, name='get_chapter'),
     path('get_Book/<str:book>/', views.get_book, name='get_book'),
+    # path('get_GPT_Book/<str:book>/', views.get_gpt_book, name='get_gpt_book'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_verse(request):
@@ -9,7 +13,7 @@ def get_verse(request):
     
 def get_chapter(request, book, chapter):
     API_ENDPOINT = "https://api.esv.org/v3/passage/text/"
-    API_KEY = "e2e819002a1d6ca90a0f84e0ff26826ad30fbe1a" 
+    API_KEY = os.environ['ESV_API_KEY'] 
 
     headers = {
         "Authorization": "Token " + API_KEY
@@ -21,7 +25,7 @@ def get_chapter(request, book, chapter):
 
 def get_book(request, book):
     API_ENDPOINT = "https://api.esv.org/v3/passage/text/"
-    API_KEY = "e2e819002a1d6ca90a0f84e0ff26826ad30fbe1a" 
+    API_KEY = os.environ['ESV_API_KEY'] 
 
     headers = {
         "Authorization": "Token " + API_KEY
@@ -29,3 +33,5 @@ def get_book(request, book):
     response = requests.get(API_ENDPOINT, headers=headers, params={"q": f"{book}", "include-footnotes": False, "include-headings": False, "include-verse-numbers": False, "include-passage-references": False, "include-audio-link": False})
         
     return JsonResponse(response.json())
+
+# def get_gpt_book(request, book):
